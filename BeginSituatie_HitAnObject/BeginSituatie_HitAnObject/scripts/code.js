112db3bc5dd3963495b8ffeb0f668eaeddcbdb36
StrikerMikez <78357085+StrikerMikez@users.noreply.github.com>
2022-04-20 16:48:25 +0200

    Update code.js

--- BeginSituatie_HitAnObject/BeginSituatie_HitAnObject/scripts/code.js
+++ BeginSituatie_HitAnObject/BeginSituatie_HitAnObject/scripts/code.js
@@ -1,4 +1,6 @@
+
 const setup = () => {
+
     let global = {
         IMAGE_COUNT: 5, // aantal figuren
         IMAGE_SIZE: 48, // grootte van de figuur
@@ -10,9 +12,15 @@ const setup = () => {
         };
     
     let klikobject = document.getElementsByTagName("img")[0];
+    //setTimeout(timerTick, 1000);
 
     const bomAlert = () => {
         window.alert("Game over");
+    }
+
+    const timerTick = () => {
+        playGame();
+        //klikobject.addEventListener("click", playGame);
     }
 
     const verplaats = () => {
@@ -29,10 +37,43 @@ const setup = () => {
     const playGame = () => {
         verplaats();
         vervang();
+
+        herstartTimer();
     }
+
+    const herstartTimer = () => {
+        clearTimeout(global.timeoutId);
+        global.timeoutId =  setTimeout(timerTick, 2000);
+    }
+
+    /*const control = () => {
+        if(image.getAttribute("src") == "images/0.png"){
+            gameOver()
+        } else {
+            playGame();
+        }
+    }*/
 
     klikobject.addEventListener("click", playGame);
 }
 
 
 window.addEventListener("load", setup);
+
+
+/*const initialize = () => {
+    let btnOpnieuw=document.getElementById("btnOpnieuw");
+    btnOpnieuw.addEventListener("click", herstartTimer);
+    setTimeout(timerTick, 1000);
+}
+
+const timerTick = () => {
+    let output=document.getElementById("output");
+    output.innerHTML+=" tick";
+}
+
+const herstartTimer = () => {
+    setTimeout(timerTick, 1000);
+}
+
+window.addEventListener("load", initialize);*/
